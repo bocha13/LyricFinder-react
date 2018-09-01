9a93496cf4cf954afbbae5e46e61f9aa7e92cdac
ignacio bochatay <ignaciobochatay@gmail.com>
2018-09-01 20:28:55 -0300

    more components and loader

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
+
+import { Provider } from './context';
+
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <React.Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Index} />
-            </Switch>
-          </div>
-        </React.Fragment>
-      </Router>
+      <Provider>
+        <Router>
+          <React.Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Index} />
+              </Switch>
+            </div>
+          </React.Fragment>
+        </Router>
+      </Provider>
     );
   }
 }
